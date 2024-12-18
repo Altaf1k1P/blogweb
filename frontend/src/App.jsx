2f25e4d040cf29827a03119cb57cd41b19b90b6e
Altaf1k1 <altafpathan@gmail.com>
2024-12-18 17:39:31 +0530

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Footer from './components/Footer/Footer.jsx';
-import Header from './components/header/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import { Outlet } from 'react-router-dom';
 
 function App() {
